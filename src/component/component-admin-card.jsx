249efff0003cb787bcec70fd0e950fa2e0ca2f36
Zaky <syafazaqi02@gmail.com>
2025-11-11 21:02:19 +0700

    feat[ZAA] Remove Layout on profilepage and registerpage, and add button lihat profile

--- src/component/component-admin-card.jsx
+++ src/component/component-admin-card.jsx
@@ -1,6 +1,6 @@
 import Button from "./component-button";
 import { IoStorefrontOutline } from "react-icons/io5";
-import { FiMail, FiMap, FiMapPin, FiPhone } from "react-icons/fi";
+import { FiEdit, FiMail, FiMapPin, FiPhone, FiEye } from "react-icons/fi";
 
 function CardInfoUmkm({
   fotoUmkm,
@@ -20,10 +20,10 @@ function CardInfoUmkm({
           "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
         }
         alt="Foto UMKM"
-        className="w-full h-40 sm:h-48 md:h-56 lg:h-52 xl:h-60 object-cover rounded-t-[20px] transition-all duration-300"
+        className="w-full h-40 sm:h-48 md:h-56 lg:h-52 xl:h-60 object-cover rounded-t-[15px] transition-all duration-300"
       />
       <div className="px-5 py-4 space-y-2">
-        <button className="border border-[#C1D0E1] rounded-full px-7 py-0.5 text-sm text-[13px] font-bold mb-4 hover:bg-gray-50 transition">
+        <button className="border border-[#C1D0E1] rounded-full px-4 py-0.5 text-sm text-[13px] font-bold mb-4 hover:bg-gray-50 transition">
           {kategori || "Kategori UMKM"}
         </button>
         <h2 className="text-[16px] font-normal text-gray-800 leading-tight mt-[-9px]">
@@ -48,11 +48,21 @@ function CardInfoUmkm({
         </div>
         <div className="pt-3">
           <Button
-            text="Edit Info Usaha"
-            variant="filled"
+            variant="outlined"
             className="w-full !py-2 text-sm font-medium"
             onClick={onEdit}
-          />
+          >
+            <FiEdit className="size-[20px] justify-center" />
+            Edit Info UMKM
+          </Button>
+          <Button
+            variant="outlined"
+            className="w-full !py-2 mt-2 text-sm font-medium"
+            href={"/umkm/3"}
+          >
+            <FiEye className="size-[20px] justify-center" />
+            Lihat Profil UMKM
+          </Button>
         </div>
       </div>
     </div>
